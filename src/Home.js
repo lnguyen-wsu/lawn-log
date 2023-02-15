84615a2d6cd192ee94ad96fbfc27c2808f4bee75
Luan Nguyen <luanthanhnguyen912@gmail.com>
2023-02-14 21:45:47 -0500

    Added Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home  = () => {
+    const handleClick = (e) => {
+        console.log("Hello Lawn",e);
+    };
+    const handleClickAgain = (name,e) =>{
+        console.log(name + "is clicked", e.target)
+    };
     return (  
         <div className="home">
             <h2>Welcome to the Lawn House</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=>handleClickAgain("Lawn",e)}>Click me Again </button>
         </div>
     );
 }
