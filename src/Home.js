f9acc30e72b60acb2a2459d341b00074e7e97839
Luan Nguyen <luanthanhnguyen912@gmail.com>
2023-02-20 21:37:29 -0500

    updated the Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
 const Home  = () => {
-    const[name, setName] = useState("Mario");
-    const[age , SetAge] = useState(25);
-    const handleClick = () => {
-        setName(name==="Lawn"?"Mario":"Lawn");
-        SetAge(age === 25 ? 31 : 25);
-    };
-   
+    const[blogs,setBlogs] = useState([
+        {title:'Testing Lawn',body:'lorem ipsum...',author:'testing'},
+        {title:'Testing Lawn 2',body:'lorem ipsum...',author:'testing'},
+        {title:'Testing Lawn 3',body:'lorem ipsum...',author:'testing'}
+    ]);
+    
+    const handleEvent = ()=> {
+        setBlogs( {title:'Testing Lawn',body:'lorem ipsum...',author:'testing'});
+    }
     return (  
         <div className="home">
-            <h2>Welcome to the {name} House</h2>
-            <h3>He is {age}</h3>
-            <button onClick={handleClick}>Click Me</button>         
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.body}</p>
+                </div>
+            ))}
+            <button onClick={handleEvent}>Click me</button>
         </div>
     );
 }
